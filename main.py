00e5b752889642606eba9d711f9183b2c7f7e322
darrene25 <email_github_kamu@example.com>
2026-06-13 23:34:42 +0700

    fitur tukar

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 print("a + b + ")
 # fitur tambahan
-a = 10
+
+a = 10 
 b = 20
-<<<<<<< HEAD
 a += b
 b = a-b
 a = a-b
 print(a)
 print(b)
-
-=======
-c = a + b
-# file baru 2
-
-anjing = "kucing"
-p = 1
-g = 19
->>>>>>> 610a8f5b0ce8e3fd206e14c67af812301454cca8
